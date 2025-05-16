127b1be9a168c90504f1db30349bc11647991e41
jonathanAlves2000 <jonathanmatheusalves@gmail.com>
2025-05-16 12:22:57 -0300

    sqlit

--- lib/repositories/ecoDriveRepository.dart
+++ lib/repositories/ecoDriveRepository.dart
@@ -1,6 +1,16 @@
+import 'package:path/path.dart';
+import 'package:ecoDrive/settings.dart';
 import 'package:sqflite/sqflite.dart';
 
 class EcoDriveRepository{
-
+  Future<Database> _getDatabse() async{
+    return openDatabase(
+      join(await getDatabasesPath(), databaseName),
+      onCreate: (db, version) async {
+        return await db.execute(createEcoDriveTableScript);
+      },
+      version: 1,
+    );
+  }
 }
 
