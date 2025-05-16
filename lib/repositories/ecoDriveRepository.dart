d1da53eb826b65e0eac40bb5ccf90ebc6e43eed7
jonathanAlves2000 <jonathanmatheusalves@gmail.com>
2025-05-16 12:36:06 -0300

    sqlit

--- lib/repositories/ecoDriveRepository.dart
+++ lib/repositories/ecoDriveRepository.dart
@@ -1,16 +1,32 @@
-import 'package:path/path.dart';
-import 'package:ecoDrive/settings.dart';
-import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart'; // Importa funções para manipular caminhos de arquivos
+import 'package:ecoDrive/settings.dart'; // Importa constantes definidas no arquivo settings.dart
+import 'package:sqflite/sqflite.dart'; // Importa a biblioteca sqflite
+import 'package:ecoDrive/models/ecoDriveModel.dart'; // Importa a classe EcoDriveModel
 
+// Classe responsável por gerenciar o acesso ao banco de dados
 class EcoDriveRepository{
   Future<Database> _getDatabse() async{
     return openDatabase(
-      join(await getDatabasesPath(), databaseName),
+      join(await getDatabasesPath(), databaseName), // Caminho para o banco de dados
       onCreate: (db, version) async {
-        return await db.execute(createEcoDriveTableScript);
+        return await db.execute(createEcoDriveTableScript); // Cria a tabela no banco de dados
       },
-      version: 1,
+      version: 1, // Versão do banco de dados
     );
+  }
+
+  Future create(EcoDriveModel model) async{
+    try{
+      final Database db = await _getDatabse();
+      await db.insert(
+        tableName,
+        model.toMap(),
+        conflictAlgorithm: ConflictAlgorithm.replace,
+      );
+  } catch(ex) {
+      print(ex);
+      return;
+    }
   }
 }
 
