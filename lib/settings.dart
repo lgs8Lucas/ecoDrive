8640c6040fa205125f92d34d5217d48eaba02f6a
fgsLobo <fgslobo@gmail.com>
2025-05-16 19:52:13 -0300

    Simples alteração no script de criação do db

--- lib/settings.dart
+++ lib/settings.dart
@@ -1,4 +1,9 @@
 const String databaseName =  "ecoDriveDB.db"; //Criando o nome do banco de dados
 const String tableName = "ecoDriveTable"; //Criando o nome da tabela do banco de dados
-const String createEcoDriveTableScript =
-    "CREATE TABLE ecoDriveTable( id INTEGER PRIMARY KEY AUTOINCREMENT, nomeViagem TEXT, dataViagem TEXT)";
+const String createEcoDriveTableScript = """
+CREATE TABLE ecoDriveTable (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nomeViagem TEXT,
+    dataViagem TEXT
+);
+""";
